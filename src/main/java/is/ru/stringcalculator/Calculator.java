87e188beb27405903359262376daf149386d1900
Isabella Finnsdottir <isabellayr@Isabellas-MacBook-Pro.local>
2017-10-10 19:45:55 +0000

    Gradle uppsetning og fyrstu tveir lidir

---
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -0,0 +1,34 @@
+package is.ru.stringcalculator;
+
+public class Calculator {
+	public static int add(String text) {
+		if(text.equals("")) {
+			return 0;
+		}
+		else {
+			if(text.contains(",")) {
+
+				String numbers[] = text.split(",");
+				return sum(numbers);
+				//return toInt(numbers[0]) + toInt(numbers[1]);
+			}
+			return 1;
+		}
+	}
+
+	private static int toInt(String number) {
+		return Integer.parseInt(number);
+	}
+
+	private static int sum(String [] numbers) {
+		int total = 0;
+		for(String number : numbers) {
+			total += toInt(number);
+		}
+		return total;
+	}
+
+	 public static void main(String[] args) {
+
+	 }
+}
